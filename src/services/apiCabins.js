3807d81be3c5abbf0b2314e0f222476c0695e111
Anh <qanh171002@gmail.com>
2025-01-03 20:10:41 +0700

    hot-toast, react hook form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,11 +11,10 @@ export async function getCabins() {
 }
 
 export async function deleteCabin(id) {
-  const { error, data } = await supabase.from("cabins").delete().eq("id", id);
+  const { error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
     console.error(error);
     throw new Error("Cabins cannot be deleted");
   }
-  return data;
 }
