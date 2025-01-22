be1e996562b1de486c7fe6d58aa2130138516461
Anh <qanh171002@gmail.com>
2025-01-22 11:07:53 +0700

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import { Toaster } from "react-hot-toast";
 import { StyleSheetManager } from "styled-components";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,7 +34,13 @@ function App() {
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
-            <Route element={<AppLayout />}>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="dashboard" />} />
 
               <Route path="dashboard" element={<Dashboard />} />
